4444273d0a7d59ea7f6878753bf7f32632250433
Sridhar Ratnakumar <sridhar.ratna@gmail.com>
2009-03-21 09:59:23 -0700

    zipimport is slow on appengine; extract 'em all

--- import_wrapper.py
+++ import_wrapper.py
@@ -4,9 +4,17 @@ import glob
 import logging
 
 # Add 3rdparty/*.zip to sys.path so that we can import them seamlessly
-root = os.path.dirname(__file__)
-for ziplib in glob.glob(os.path.join(root, '3rdparty', '*.zip')):
-    logging.debug("Adding 3rdparty library '%s' to sys.path" % ziplib)
-    sys.path.insert(0, ziplib)
+
+#for ziplib in glob.glob(os.path.join(root, '3rdparty', '*.zip')):
+#    logging.debug("Adding 3rdparty library '%s' to sys.path" % ziplib)
+#    sys.path.insert(0, ziplib)
+
+root = os.path.abspath(os.path.dirname(__file__))
+tpty = os.path.join(root, '3rdparty')
+for child in os.listdir(tpty):
+    child = os.path.join(tpty, child)
+    if os.path.isdir(child):
+        dir = os.path.abspath(os.path.join(root, '3rdparty', child))
+        sys.path.insert(0, dir)
 
 import debris.rstcode 
